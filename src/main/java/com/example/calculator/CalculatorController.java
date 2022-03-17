8058d5e59566cd5244fb61393f7e37c53cf07ee9
Sorry4Nothing <robnoleh@gmail.com>
2022-03-17 16:02:33 +0100

    added onklicks

--- src/main/java/com/example/calculator/CalculatorController.java
+++ src/main/java/com/example/calculator/CalculatorController.java
@@ -4,11 +4,101 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class CalculatorController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void btnzeroclicked() {
+        var one = 1;
     }
+
+    @FXML
+    private void btnoneclicked() {
+        var one = 1;
+    }
+
+    @FXML
+    private void btntwoclicked() {
+        var one = 1;
+    }
+
+    @FXML
+    private void btnthreeclicked() {
+        var one = 1;
+    }
+
+    @FXML
+    private void btnfourclicked() {
+        var one = 1;
+    }
+
+    @FXML
+    private void btnfiveclicked() {
+        var one = 1;
+    }
+
+    @FXML
+    private void btnsixclicked() {
+        var one = 1;
+    }
+
+    @FXML
+    private void btnsevenclicked() {
+        var one = 1;
+    }
+
+    @FXML
+    private void btneightclicked() {
+        var one = 1;
+    }
+
+    @FXML
+    private void btnnineclicked() {
+        var one = 1;
+    }
+
+    @FXML
+    private void btnplusclicked() {
+        var one = 1;
+    }
+
+    @FXML
+    private void btnminusclicked() {
+        var one = 1;
+    }
+
+    @FXML
+    private void btntimesclicked() {
+        var one = 1;
+    }
+
+    @FXML
+    private void btndevidedclicked() {
+        var one = 1;
+    }
+
+    @FXML
+    private void btndotclicked() {
+        var one = 1;
+    }
+
+    @FXML
+    private void btnclearclicked() {
+        var one = 1;
+    }
+
+    @FXML
+    private void btnsqrtclicked() {
+        var one = 1;
+    }
+
+    @FXML
+    private void btntoggleclicked() {
+        var one = 1;
+    }
+
+    @FXML
+    private void btnequalsclicked() {
+        var one = 1;
+    }
+
+
 }
