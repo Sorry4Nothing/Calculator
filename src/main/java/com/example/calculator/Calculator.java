0ebe04c18ab936361d4c10017157ad1342a9c644
Sorry4Nothing <robnoleh@gmail.com>
2022-03-17 15:15:01 +0100

    added new fxml

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -11,7 +11,7 @@ public class Calculator extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Calculator.class.getResource("calculator.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Calculator");
         stage.setScene(scene);
         stage.show();
